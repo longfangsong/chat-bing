c6b569c67584e269b8cb65affed8a374aad5ccfc
longfangsong <longfangsong@icloud.com>
2023-04-07 01:00:36 +0800

    style and direct reply support

--- api/webhook.ts
+++ api/webhook.ts
@@ -10,18 +10,55 @@ function encrypt(secret: string, data: string): string {
     return result;
 }
 
+interface ReplyToMessageFrom {
+    id: number,
+}
+
+interface ReplyToMessage {
+    message_id: number,
+    from: ReplyToMessageFrom,
+}
+
+interface Chat {
+    id: number,
+    type: string,
+}
+
+interface Message {
+    chat: Chat,
+    text: string,
+    message_id: number,
+    reply_to_message: ReplyToMessage,
+}
+
+function isReplyToMe(reply_to_message: ReplyToMessage): boolean {
+    const self_id = parseInt(process.env.TELEGRAM_TOKEN?.split(':')[0]!!);
+    return reply_to_message?.from?.id == self_id;
+}
 
 export default async ({ body }, response) => {
     try {
         const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
         let { chat: { id, type }, text, message_id, reply_to_message } = body.message;
-        if ((type === 'group' || type == 'supergroup') && !text.startsWith('@MFGWBot')) {
+        const isFromGroupChat = type === 'group' || type == 'supergroup';
+        if (isFromGroupChat && !text.startsWith('@MFGWBot') && !isReplyToMe(reply_to_message)) {
             response.status(200).send('Group message not aim to me');
             return;
-        } else if (type === 'group' || type == 'supergroup') {
+        } else if (isFromGroupChat) {
             text = text.replace('@MFGWBot', '').trim();
         }
-        await send_request(id, message_id, reply_to_message?.message_id, text);
+        let style = "";
+        if (text.startsWith('/creative')) {
+            style = 'creative';
+            text = text.replace('/creative', '').trim();
+        } else if (text.startsWith('/balanced')) {
+            style = 'balanced';
+            text = text.replace('/balanced', '').trim();
+        } else if (text.startsWith('/precise')) {
+            style = 'precise';
+            text = text.replace('/precise', '').trim();
+        }
+        await send_request(id, style, message_id, reply_to_message?.message_id, text);
         response.status(200).send('Message received');
     } catch (e) {
         console.error(e);
@@ -30,12 +67,13 @@ export default async ({ body }, response) => {
 }
 
 const message_file_url = 'https://api.github.com/repos/longfangsong/action-ask-bing/contents/request.json.encrypted';
-async function send_request(chat_id: number, message_id: number, reply_to_message_id: number | null, text: string) {
+async function send_request(chat_id: number, style: string, message_id: number, reply_to_message_id: number | null, text: string) {
     const file_content = {
         "chat_id": chat_id,
         "reply_to_message_id": reply_to_message_id,
         "message_id": message_id,
-        "question": text
+        "question": text,
+        "style": style
     };
     const currentFileInfoResponse = await fetch(message_file_url, {
         headers: {
